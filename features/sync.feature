75d38ed235c39c7c30fed6c6ada6c8b42a3646bc
David McKay <david@rawkode.dev>
2021-01-19 22:43:36 +0000

    feat: implement the other bdd scenarios as tests
    The final assertion is failing and that's because I'm using `clone` to veirfy `sync`
    
    Now I need to provide the sync wrapper

--- features/sync.feature
+++ features/sync.feature
@@ -10,39 +10,38 @@ Feature: Bootstrap Git Repository
         Given I have no directory called "gitsync"
         When I sync the "https://gitlab.com/rawkode/gitsync" repository
         Then the repository is cloned into the "gitsync" directory
-        And the bootstrapping completes
+        And the sync completes
 
     @CLI
     Example: Local directory isn't a Git repository
 
-        Given I have a directory called "gitsync"
-        And it is not a Git repository
+        Given I have a non-empty directory called "gitsync"
         When I sync the "https://gitlab.com/rawkode/gitsync" repository
         Then the directory is left untouched
-        And the bootstrapping errors
+        And the sync errors
 
     Rule: If we have a local clone the origin remote must be correct
 
         Example: Local clone has incorrect url for origin remote
 
-            Given I have local clone called "gitsync"
+            Given I have a Git repository in a directory called "gitsync"
             But it has a remote called "origin" that points to "https://gitlab.com/rawkode/dotfiles"
             When I sync the "https://gitlab.com/rawkode/gitsync" repository
             Then the directory is left untouched
-            And the bootstrapping errors
+            And the sync errors
 
         Example: Local clone has no remote called "origin"
 
-            Given I have local clone called "gitsync"
+            Given I have a Git repository in a directory called "gitsync"
             But it has no remote called "origin"
             When I sync the "https://gitlab.com/rawkode/gitsync" repository
             Then the directory is left untouched
-            And the bootstrapping errors
+            And the sync errors
 
 
         Example: Local clone has the correct origin remote
 
-            Given I have local clone called "gitsync"
+            Given I have a Git repository in a directory called "gitsync"
             And it has a remote called "origin" that points to "https://gitlab.com/rawkode/gitsync"
             When I sync the "https://gitlab.com/rawkode/gitsync" repository
-            Then the bootstrapping completes
+            Then the sync completes
--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -8,6 +8,7 @@ pub struct CucumberState {
     test_dir: PathBuf,
     clone_dir: PathBuf,
     repo_url: String,
+    sync_error: Option<git2::Error>,
 }
 
 #[async_trait(?Send)]
@@ -19,6 +20,7 @@ impl World for CucumberState {
             test_dir: TempDir::new("gitsync-test-").unwrap().into_path(),
             repo_url: String::from(""),
             clone_dir: PathBuf::new(),
+            sync_error: None,
         })
     }
 }
@@ -27,6 +29,7 @@ mod example_steps {
     use crate::CucumberState;
     use cucumber::gherkin::Step;
     use cucumber::Steps;
+    use futures::future::join;
     use std::time::Duration;
     use std::{path::PathBuf, rc::Rc};
 
@@ -38,17 +41,52 @@ mod example_steps {
                 r#"^I have no directory called "(\S+)"$"#,
                 i_have_no_directory,
             )
+            .given_regex(
+                r#"I have a non-empty directory called "(\S+)"$"#,
+                i_have_a_non_empty_directory,
+            )
+            .given_regex(
+                r#"I have a Git repository in a directory called "(\S+)"$"#,
+                i_have_a_git_repository,
+            )
+            .given_regex(
+                r#"it has a remote called "(\S+)" that points to "([^"]+)"$"#,
+                it_has_remote,
+            )
+            .given_regex(r#"it has no remote called "(\S+)"$"#, do_nothing_regex)
             .when_regex(r#"I sync the "([^"]+)" repository$"#, sync_git_repository)
             .then_regex(
                 r#"the repository is cloned into the "(\S+)" directory$"#,
                 repository_is_cloned,
             )
-            .then("the bootstrapping completes", do_nothing);
+            .then("the directory is left untouched", directory_left_untouched)
+            .then("the sync completes", sync_is_ok)
+            .then("the sync errors", sync_errors);
 
         builder
     }
 
     fn do_nothing(world: CucumberState, _step: Rc<Step>) -> CucumberState {
+        world
+    }
+
+    fn do_nothing_regex(
+        world: CucumberState,
+        _matches: Vec<String>,
+        _step: Rc<Step>,
+    ) -> CucumberState {
+        world
+    }
+
+    fn sync_is_ok(world: CucumberState, _step: Rc<Step>) -> CucumberState {
+        assert_eq!(true, world.sync_error.is_none());
+
+        world
+    }
+
+    fn sync_errors(world: CucumberState, _step: Rc<Step>) -> CucumberState {
+        assert_eq!(true, world.sync_error.is_some());
+
         world
     }
 
@@ -62,6 +100,66 @@ mod example_steps {
         assert_eq!(false, path.exists());
 
         world.clone_dir = path;
+
+        world
+    }
+
+    fn i_have_a_non_empty_directory(
+        mut world: CucumberState,
+        matches: Vec<String>,
+        _step: Rc<Step>,
+    ) -> CucumberState {
+        let path: PathBuf = PathBuf::from(&world.test_dir).join(matches.get(1).unwrap());
+
+        let _ = std::fs::create_dir(path.clone());
+
+        assert_eq!(true, path.is_dir());
+
+        let _ = std::fs::File::create(path.join("file.txt"));
+
+        assert_eq!(true, path.join("file.txt").is_file());
+
+        world.clone_dir = path;
+
+        world
+    }
+
+    fn i_have_a_git_repository(
+        mut world: CucumberState,
+        matches: Vec<String>,
+        _step: Rc<Step>,
+    ) -> CucumberState {
+        let clone_dir: PathBuf = world.test_dir.clone().join(matches.get(1).unwrap());
+
+        let output = std::process::Command::new("git")
+            .arg("init")
+            .arg(&clone_dir)
+            .status()
+            .expect("Failed to initialise test repository");
+
+        assert_eq!(true, output.success());
+
+        world.clone_dir = clone_dir.clone();
+
+        let _ = std::fs::File::create(clone_dir.join("file.txt"));
+
+        world
+    }
+
+    fn it_has_remote(world: CucumberState, matches: Vec<String>, _step: Rc<Step>) -> CucumberState {
+        let name: &String = matches.get(1).unwrap();
+        let url: PathBuf = world.clone_dir.clone();
+
+        let output = std::process::Command::new("git")
+            .current_dir(&world.clone_dir)
+            .arg("remote")
+            .arg("add")
+            .arg(&name)
+            .arg(&url)
+            .status()
+            .expect("Failed to add remote to test repository");
+
+        assert_eq!(true, output.success());
 
         world
     }
@@ -82,11 +180,14 @@ mod example_steps {
             passphrase: None,
         };
 
-        gitsync
-            .clone_repository()
-            .expect("Failed to clone repository");
-
         world.repo_url = repo_url;
+
+        let sync_error = match gitsync.clone_repository() {
+            Ok(_) => None,
+            Err(e) => Some(e),
+        };
+
+        world.sync_error = sync_error;
 
         world
     }
@@ -111,6 +212,15 @@ mod example_steps {
             format!("{}\n", repo_url).as_bytes(),
             output.stdout.as_slice()
         );
+
+        world
+    }
+
+    fn directory_left_untouched(world: CucumberState, _step: Rc<Step>) -> CucumberState {
+        let path: PathBuf = PathBuf::from(&world.test_dir).join(&world.clone_dir);
+
+        assert_eq!(true, path.is_dir());
+        assert_eq!(true, path.join("file.txt").is_file());
 
         world
     }
